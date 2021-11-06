1d9f57ae860eb535a073171a7c2671ce8c371cac
boviex <boviex@users.noreply.github.com>
2021-11-06 19:55:49 +1100

    Update gbafe.h

--- include/gbafe.h
+++ include/gbafe.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-#include "global.h"
+// #include "global.h"
 
 #include "gba/gba.h"
 
@@ -54,6 +54,28 @@ extern "C" {
 #include "uiutils.h"
 #include "unit_icon_data.h"
 #include "unit_icon_pointer.h"
+
+#define CONST_DATA __attribute__((section(".data")))
+
+#include "types.h"
+#include "variables.h"
+#include "functions.h"
+
+// helper macros
+
+#define ARRAY_COUNT(array) (sizeof(array) / sizeof((array)[0]))
+
+#define RED_VALUE(color) ((color) & 0x1F)
+#define GREEN_VALUE(color) (((color) >> 5) & 0x1F)
+#define BLUE_VALUE(color) (((color) >> 10) & 0x1F)
+
+#define ABS(aValue) ((aValue) >= 0 ? (aValue) : -(aValue))
+
+#define SIN(aAngle) (gSinLookup[(aAngle&0xFF)])
+#define COS(aAngle) (gSinLookup[0x40 + (aAngle&0xFF)])
+
+#define RECT_DISTANCE(aXA, aYA, aXB, aYB) (ABS((aXA) - (aXB)) + ABS((aYA) - (aYB)))
+
 
 
 #ifdef __cplusplus
